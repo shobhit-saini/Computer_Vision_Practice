0bcfe9298c61d45ffb191c073f24b0974346a8dd
SHOBHIT <shobhitsaini95@gmail.com>
2020-01-15 02:54:53 +0530

    Add files via upload

---
+++ Show_Date_and_Time_on_videos.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 23:07:28 2020
+
+@author: SHOBHIT SAINI
+"""
+
+import cv2
+import datetime
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        datet = str(datetime.datetime.now())
+        frame = cv2.putText(frame, datet, (10, 50), font, 1, 
+                            (0,255,255), 2, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    else:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
